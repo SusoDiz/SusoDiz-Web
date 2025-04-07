e99cf46003aa48c66e872fc954ef0e833d410765
SusoDiz <jesusgonzalezdiaz77@gmail.com>
2025-04-07 17:35:10 +0100

    Delete /dist Cloudflare

--- dist/suso-diz-web/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a as r,f as e,g as t,i as o,j as p,k as m}from"./chunk-UDX3YEI5.mjs";import"./chunk-S6KH3LOX.mjs";import a from"./angular-app-manifest.mjs";e(a);export{m as default,p as \u0275destroyAngularServerApp,t as \u0275extractRoutesAndCreateRouteTree,o as \u0275getOrCreateAngularServerApp,r as \u0275resetCompiledComponents};
