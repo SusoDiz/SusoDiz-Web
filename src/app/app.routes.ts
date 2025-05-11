a9b6d1fc6c53429242dc7f194bf14d9d14b5ba79
SusoDiz <jesusgonzalezdiaz77@gmail.com>
2025-05-11 16:42:55 +0100

    Proyectos, rutas e imagenes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { Threshold95Component } from './projects/threshold-95/threshold-95.component'; // Importa el componente
+import { HomelabComponent } from './projects/homelab/homelab.component'; // Importa el componente
 
 export const routes: Routes = [
     { path: '', component: HomeComponent }, // Página de inicio
     { path: 'about', component: AboutComponent }, // Página About
     { path: 'projects', component: ProjectsComponent }, // Página Projects
     { path: 'projects/threshold-95', component: Threshold95Component }, // Nueva ruta para el Umbral del 95%
+    { path: 'projects/homelab', component: HomelabComponent }, // Página de Homelab
 ];
---
+++ src/app/projects/homelab/homelab.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-homelab',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './homelab.component.html',
+  styleUrl: './homelab.component.scss'
+})
+export class HomelabComponent {
+  
+}
