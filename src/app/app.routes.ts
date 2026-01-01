2806dbd1484da0efe7f17840bd9f6880934f21c9
SusoDiz <dev@susodiz.com>
2026-01-01 05:04:31 +0000

    SusoDiz en SusoDiz

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,20 @@
 import { Routes } from '@angular/router';
-import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
+import { Error404Component } from './error404/error404.component'; 
+import { SusoDizComponent } from './susodiz/susodiz.component';
+import { AboutComponent } from './about/about.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { LaligagateComponent } from './laligagate/laligagate.component';
-import { Threshold95Component } from './projects/threshold-95/threshold-95.component'; // Importa el componente
-import { HomelabComponent } from './projects/homelab/homelab.component'; // Importa el componente
-import { Error404Component } from './error404/error404.component'; // Importa el componente de error
+import { Threshold95Component } from './projects/threshold-95/threshold-95.component'; 
+import { HomelabComponent } from './projects/homelab/homelab.component'; 
 
 export const routes: Routes = [
     { path: '', component: HomeComponent, data: { animation: 'HomePage' } }, // Página de inicio
+    { path: 'susodiz', component: SusoDizComponent }, // Más SusoDiz
     { path: 'about', component: AboutComponent, data: { animation: 'AboutPage' } }, // Página About
     { path: 'projects', component: ProjectsComponent, data: { animation: 'ProjectsPage' } }, // Página Projects
     { path: 'laligagate', component: LaligagateComponent, data: { animation: 'LaligagatePage' } }, // Página LaLigaGate
     { path: 'projects/threshold-95', component: Threshold95Component, data: { animation: 'Threshold95Page' } }, // Nueva ruta para el Umbral del 95%
     { path: 'projects/homelab', component: HomelabComponent, data: { animation: 'HomelabPage' } }, // Página de Homelab
-    { path: '**', component: Error404Component } // Ruta comodín para páginas no encontradas
+    { path: '**', component: Error404Component }, // Ruta comodín para páginas no encontradas (¡¡¡SIEMPRE AL FINAL!!!)
 ];
---
+++ src/app/susodiz/susodiz.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-susodiz',
+  imports: [RouterModule],
+  templateUrl: './susodiz.component.html',
+  styleUrl: './susodiz.component.scss'
+})
+export class SusoDizComponent {
+
+}
